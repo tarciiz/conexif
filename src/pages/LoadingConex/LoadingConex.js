2b6ad730b3bfcc7df55af0b3b4c00785fd740688
tarciiz <sidtarcisiosid88@gmail.com>
2023-05-14 14:06:06 -0300

    first commit

---
+++ src/pages/LoadingConex/LoadingConex.js
@@ -0,0 +1,34 @@
+import './LoadingConex.css';
+
+function LoadingConex() {
+    return (
+      <div className="LoadingDiv">
+          <div className="Video">
+            <video autoPlay loop muted>
+                <source src={process.env.PUBLIC_URL +"/conex-anim.mp4"} type="video/mp4" />
+
+            </video>
+            <br/>
+            <span className="TextLoad">LOADING
+                <span class="dot-1">.</span>
+                <span class="dot-2">.</span>
+                <span class="dot-3">.</span>
+            </span>
+            <br/>
+            <br/>
+            <div>
+                <span className="Follow">SIGA-NOS</span>
+                <br/>
+                <br/>
+
+                <a className="Insta" href="https://www.instagram.com/conexif" target="_self" rel="noreferrer">
+                    <i class="fab fa-instagram"></i>
+                </a>
+            </div>
+          </div>
+      </div>
+    );
+  }
+
+export default LoadingConex;
+  
