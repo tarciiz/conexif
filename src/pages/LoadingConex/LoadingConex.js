305d53bc20b5b01e248f6f0651c7aaa26b869889
tarciiz <sidtarcisiosid88@gmail.com>
2023-05-14 15:34:55 -0300

    v

--- src/pages/LoadingConex/LoadingConex.js
+++ src/pages/LoadingConex/LoadingConex.js
@@ -11,6 +11,18 @@ function LoadingConex() {
             video.play();
         }
      }, [])
+
+     const openInstagramApp = (username)=> {
+      // Check if the Instagram app is installed
+      if (/(iPhone|iPod|iPad)/i.test(navigator.userAgent)) {
+        window.location.href = 'instagram://user?username=${username}';
+      } else if (/Android/i.test(navigator.userAgent)) {
+        window.location.href = 'intent://instagram/#Intent;package=com.instagram.android;scheme=https;end';
+      } else {
+        // Fallback URL for other platforms or when the app is not installed
+        window.open('https://www.instagram.com/', '_self');
+      }
+    }
     
     return (
       <div className="LoadingDiv">
@@ -33,7 +45,7 @@ function LoadingConex() {
                 <br/>
                 <br/>
 
-                <a className="Insta" href="https://www.instagram.com/conexif" target="_self" rel="noreferrer">
+                <a className="Insta" href="#" onClick={openInstagramApp('conexif')}target="_self" rel="noreferrer">
                     <i class="fab fa-instagram"></i>
                 </a>
             </div>
