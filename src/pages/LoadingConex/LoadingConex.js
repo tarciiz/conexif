dae0551885a71c6fad2f58aa06188ff1d6745604
tarciiz <sidtarcisiosid88@gmail.com>
2023-05-14 15:42:03 -0300

    t

--- src/pages/LoadingConex/LoadingConex.js
+++ src/pages/LoadingConex/LoadingConex.js
@@ -33,8 +33,11 @@ function LoadingConex() {
                 <br/>
                 <br/>
 
-                <a className="Insta" href="https://www.instagram.com/conexif" target="_self" rel="noreferrer">
+                <a className="Insta" href="https://www.instagram.com/conexif" target="_blank" rel="noreferrer">
                     <i class="fab fa-instagram"></i>
+                </a>
+                <a className="Insta" href="https://t.me/groupname" target="_blank" rel="noreferrer">
+                    <i class="fab fa-telegram telegram-icon"></i>
                 </a>
             </div>
           </div>
