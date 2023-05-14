80bcd75f25aa96a8d14b8f5fc25313955fb40756
tarciiz <sidtarcisiosid88@gmail.com>
2023-05-14 14:46:59 -0300

    added gif

--- src/pages/LoadingConex/LoadingConex.js
+++ src/pages/LoadingConex/LoadingConex.js
@@ -4,10 +4,7 @@ function LoadingConex() {
     return (
       <div className="LoadingDiv">
           <div className="Video">
-            <video autoPlay loop muted>
-                <source src={process.env.PUBLIC_URL +"/conex-anim.mp4"} type="video/mp4" />
-                <p style={{'color':'white'}}>Your browser does not support the video tag.</p>
-            </video>
+            <img  src={process.env.PUBLIC_URL +"/conex-anim.gif"} />
             <br/>
             <span className="TextLoad">LOADING
                 <span class="dot-1">.</span>
